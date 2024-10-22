95cebb9a6474698e0f808c6b1a135ff38f43baf8
mansi <mansithakur.obdure@gmail.com>
2024-10-23 03:16:30 +0530

    contact us component

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
-import Priority from "@/components/priority";
-import { Services } from "@/components/services";
-import { Partners } from "~/components";
+import { ContactUs } from '@/components/contactUs/contactUs';
+import Priority from '@/components/priority';
+import { Services } from '@/components/services';
+import { Partners } from '~/components';
 
 export default function Home() {
-    return (
-        <>
-    <Partners />
-    <Services/>
-    <Priority/>
+  return (
+    <>
+      <ContactUs />
+      <Partners />
+      <Services />
+      <Priority />
     </>
-
-)
+  );
 }
