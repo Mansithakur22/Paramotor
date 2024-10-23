ee9b955826cad0bdc83a56678289ddd81c59fc13
mansi <mansithakur.obdure@gmail.com>
2024-10-23 12:57:56 +0530

    manage money

--- src/components/ourPriority/ourPriority.jsx
+++ src/components/ourPriority/ourPriority.jsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 export function OurPriority() {
   return (
     <section>
-      <div className="py-24 text-center bg-blue-950"> {/* Adjust padding */}
+      <div className="py-24 text-center bg-blue-950">
         <div className="flex flex-col items-center mx-auto">
           <Image 
             src="/images/yoursecurityourpriority/security-priority.png" 
-            alt="Security Priority" // Provide meaningful alt text
-            width={50} // Adjust width for better visibility
-            height={50} // Adjust height for better visibility
-            className="mb-4" // Add margin below the image
+            alt="Security Priority"
+            width={50} 
+            height={50} 
+            className="mb-4"
           />
-          <h2 className="mt-4 mb-3 text-white text-4xl font-semibold">Your security. Our priority.</h2> {/* Increased text size */}
+          <h2 className="mt-4 mb-3 text-white text-4xl font-semibold">Your security. Our priority.</h2> 
           <p className="text-white">
             We always have your security in mind. Rest easy knowing your data is
             protected with 128-bit encryption.
